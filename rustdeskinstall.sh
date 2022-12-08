3943c4dd5ade22b33f97815164cfd9cb4a34db28
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2022-12-08 23:25:38 +0700

    Update rustdeskinstall.sh

--- rustdeskinstall.sh
+++ rustdeskinstall.sh
@@ -114,9 +114,9 @@ cd /opt/rustdesk/ || exit 1
 #Download latest version of Rustdesk
 RDLATEST=$(curl https://api.github.com/repos/rustdesk/rustdesk-server/releases/latest -s | grep "tag_name"| awk '{print substr($2, 2, length($2)-3) }')
 
-#tim phien ban bang tay:  https://github.com/rustdesk/rustdesk-server/releases  --> Download > version 1.1.6: 
-wget "https://github.com/rustdesk/rustdesk-server/releases/download/${RDLATEST}/rustdesk-server-linux-x64.zip"
-unzip rustdesk-server-linux-x64.zip
+#tim phien ban bang tay:  https://github.com/rustdesk/rustdesk-server/releases  --> Download > version 1.1.6-2: 
+wget "https://github.com/rustdesk/rustdesk-server/releases/download/${RDLATEST}/rustdesk-server-linux-amd64.zip"
+unzip rustdesk-server-linux-amd64.zip
 
 # Make Folder /var/log/rustdesk/
 if [ ! -d "/var/log/rustdesk" ]; then
